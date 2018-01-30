d25ceb961f38e202ceb492a41cc7971008b9bd18
Samuel Husso <shusso@iki.fi>
2018-01-30 06:42:57 +0200

    persistence: simplify engine selection, pass engine in conf

--- freqtrade/persistence.py
+++ freqtrade/persistence.py
@@ -18,29 +18,36 @@ _CONF = {}
 _DECL_BASE = declarative_base()
 
 
-def init(config: dict, engine: Optional[Engine] = None) -> None:
+def set_engine_path(engine_path: str, engine_type='sqlite') -> str:
+    pass
+
+
+def init(config: dict) -> None:
     """
     Initializes this module with the given config,
     registers all known command handlers
     and starts polling for message updates
     :param config: config to use
-    :param engine: database engine for sqlalchemy (Optional)
     :return: None
     """
+
     _CONF.update(config)
-    if not engine:
-        if _CONF.get('dry_run', False):
-            # the user wants dry run to use a DB
-            if _CONF.get('dry_run_db', False):
-                engine = create_engine('sqlite:///tradesv3.dry_run.sqlite')
+    path = _CONF.get('sqlite_path')
+    
+
+    if _CONF.get('dry_run', False):
+        # the user wants dry run to use a DB
+        if _CONF.get('dry_run_db', False):
+            engine = create_engine('sqlite:///tradesv3.dry_run.sqlite')
             # Otherwise dry run will store in memory
-            else:
-                engine = create_engine('sqlite://',
-                                       connect_args={'check_same_thread': False},
-                                       poolclass=StaticPool,
-                                       echo=False)
         else:
-            engine = create_engine('sqlite:///tradesv3.sqlite')
+            engine = create_engine('sqlite://',
+                                   connect_args={'check_same_thread': False},
+                                   poolclass=StaticPool,
+                                   echo=False)
+    else:
+        engine = create_engine('sqlite:///tradesv3.sqlite')
+
 
     session = scoped_session(sessionmaker(bind=engine, autoflush=True, autocommit=True))
     Trade.session = session()
